94bf1fc57146212af46125edf4ce6866c8aa43da
jbencook <ben@sparrow.dev>
2022-05-04 21:51:31 +0000

    make dirs

--- sparrow_patterns/github/github.py
+++ sparrow_patterns/github/github.py
@@ -13,6 +13,7 @@ def github(project_directory: str = ".") -> None:
     template_directory = Path(__file__).parent / "templates"
     output_directory = Path(project_directory) / ".github/workflows"
     filename = "build.yml"
+    output_directory.mkdir(parents=True, exist_ok=True)
     with open(template_directory / filename) as f:
         file_string = f.read()
     with open(output_directory / filename, "w") as f:
--- sparrow_patterns/vscode/vscode.py
+++ sparrow_patterns/vscode/vscode.py
@@ -13,6 +13,7 @@ def vscode(project_directory: str = ".") -> None:
     template_directory = Path(__file__).parent / "templates"
     output_directory = Path(project_directory) / ".vscode"
     filename = "launch.json"
+    output_directory.mkdir(exist_ok=True)
     with open(template_directory / filename) as f:
         file_string = f.read()
     with open(output_directory / filename, "w") as f:
