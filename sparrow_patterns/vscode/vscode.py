7f2d346326fd0fb6683891b9970f9de7ffd46e2d
jbencook <ben@sparrow.dev>
2022-05-04 21:01:44 +0000

    add vscode

--- sparrow_patterns/vscode/vscode.py
+++ sparrow_patterns/vscode/vscode.py
@@ -1,18 +1,18 @@
 from pathlib import Path
 
 
-def github(project_directory: str = ".") -> None:
+def vscode(project_directory: str = ".") -> None:
     """
-    Write a .github directory for the project.
+    Write a .vscode directory for the project.
 
     Parameters
     ----------
     project_directory
-        Where to create the .github folder. Defaults to working directory.
+        Where to create the .vscode folder. Defaults to working directory.
     """
     template_directory = Path(__file__).parent / "templates"
-    output_directory = Path(project_directory) / ".vscode"
-    filename = "launch.json"
+    output_directory = Path(project_directory) / ".github/workflows"
+    filename = "build.yml"
     with open(template_directory / filename) as f:
         file_string = f.read()
     with open(output_directory / filename, "w") as f:
