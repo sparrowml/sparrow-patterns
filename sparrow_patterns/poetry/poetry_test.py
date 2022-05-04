bf01168625c6f78fcf8074896abf5b3c46a37182
jbencook <ben@sparrow.dev>
2022-05-04 20:27:45 +0000

    fix test

--- sparrow_patterns/poetry/poetry_test.py
+++ sparrow_patterns/poetry/poetry_test.py
@@ -4,18 +4,14 @@ import tempfile
 from .poetry import poetry
 
 
-def test_project_name_converts_to_source_directory():
+def test_adds_optional_packages():
     with tempfile.TemporaryDirectory() as project_directory:
         poetry(
             project_name="foo-bar",
-            package=True,
+            gpu=True,
             project_directory=project_directory,
         )
-        source_directory = "foo_bar"
-        env_example_path = os.path.join(project_directory, ".devcontainer/.env.example")
-        gitignore_path = os.path.join(project_directory, ".devcontainer/.gitignore")
-        assert os.path.exists(env_example_path) == False
-        assert os.path.exists(gitignore_path) == False
-        config_path = os.path.join(project_directory, ".devcontainer/devcontainer.json")
-        with open(config_path) as f:
-            assert source_directory in f.read()
+        pyproject_path = os.path.join(project_directory, "pyproject.toml")
+        assert os.path.exists(pyproject_path)
+        with open(pyproject_path) as f:
+            assert "torchvision" in f.read()
