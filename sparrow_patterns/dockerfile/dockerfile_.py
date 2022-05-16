74e6728a9246448dc2add68422f1d700816ccdeb
jbencook <ben@sparrow.dev>
2022-05-16 17:04:55 +0000

    support deepstream docker image

--- sparrow_patterns/dockerfile/dockerfile_.py
+++ sparrow_patterns/dockerfile/dockerfile_.py
@@ -6,6 +6,7 @@ from jinja2 import Environment
 
 def dockerfile(
     gpu: bool = False,
+    deepstream: bool = False,
     project_directory: str = ".",
 ) -> None:
     """
@@ -15,13 +16,15 @@ def dockerfile(
     ----------
     gpu
         Whether to make the GPU available
+    deepstream
+        Whether to set up project for DeepStream
     project_directory
         Where to create the .devcontainer folder. Defaults to working directory.
     """
     env = Environment(autoescape=True)
     template_directory = Path(__file__).parent / "templates"
     output_directory = Path(project_directory)
-    template_variables = dict(gpu=gpu)
+    template_variables = dict(gpu=gpu, deepstream=deepstream)
     for fname in os.listdir(template_directory):
         output_path = output_directory / fname
         with open(template_directory / fname) as f:
