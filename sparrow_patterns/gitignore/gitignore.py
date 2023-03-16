3384b48267cdfedd58a22d379f4852d85f2675e3
jbencook <ben@sparrow.dev>
2023-03-16 20:26:12 +0000

    dont ignore gitignore

--- sparrow_patterns/gitignore/gitignore.py
+++ sparrow_patterns/gitignore/gitignore.py
@@ -12,9 +12,10 @@ def gitignore(project_directory: str = ".") -> None:
     """
     template_directory = Path(__file__).parent / "templates"
     output_directory = Path(project_directory)
-    filename = ".gitignore"
+    read_filename = "gitignore"
+    write_filename = f".{read_filename}"
     output_directory.mkdir(exist_ok=True)
-    with open(template_directory / filename) as f:
+    with open(template_directory / read_filename) as f:
         file_string = f.read()
-    with open(output_directory / filename, "w") as f:
+    with open(output_directory / write_filename, "w") as f:
         f.write(file_string)
